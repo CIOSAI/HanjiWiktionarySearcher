a720e669508a39268fff0e80fdb611cecb9e43b3
CIOSAI <jyinteractive.tw@gmail.com>
2022-06-19 01:07:09 +0800

    hook up textbox and searchspan

--- src/Interact.tsx
+++ src/Interact.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Searcher} from './SearchSpan'
 
 export function Interact() {
-
+  const [textArray, setTextArray] = useState([""]);
 
   return (
     <div>
       <h1>輸入漢字</h1>
-      <input type={'text'}></input>
+      <input type={'text'} onKeyDown={(evt) => {
+          if(evt.key=='Enter') setTextArray(evt.currentTarget.value.split(""));
+        }}></input>
       <div>
-        <Searcher character='著'></Searcher>
-        <Searcher character='大'></Searcher>
+        {textArray.map(v=>{
+          return <Searcher key={v} character={v}></Searcher>
+        })}
       </div>
     </div>
   );
