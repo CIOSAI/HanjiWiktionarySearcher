2c308918e17d74abbd7f564576f3aa2d279164d3
CIOSAI <jyinteractive.tw@gmail.com>
2022-06-20 20:34:48 +0800

    added language select buttons (doesnt work yet)

--- src/Interact.tsx
+++ src/Interact.tsx
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Searcher} from './SearchSpan'
+import { languageCode } from './LanguageItemReader'
 
 export function Interact() {
   const [textArray, setTextArray] = useState([""]);
+  const [languageOptions, setLanguageOptions] = useState([<p key={"wtf bro"}></p>]);
+
+  useEffect(()=>{
+
+    let toSetLanguageOptions = []
+    for(let i of Array.from(languageCode.keys())){
+      toSetLanguageOptions.push(
+        <div key={i}>
+          <input type={"checkbox"} name={i}></input>
+          <label>{languageCode.get(i)}</label>
+        </div>
+      )
+    }
+    setLanguageOptions(toSetLanguageOptions)
+
+  }, [])
 
   return (
     <div>
@@ -10,6 +27,7 @@ export function Interact() {
       <input type={'text'} onKeyDown={(evt) => {
           if(evt.key=='Enter') setTextArray(evt.currentTarget.value.split(""));
         }}></input>
+      <ul>{languageOptions}</ul>
       <div>
         {textArray.map(v=>{
           return <Searcher key={v} character={v}></Searcher>
